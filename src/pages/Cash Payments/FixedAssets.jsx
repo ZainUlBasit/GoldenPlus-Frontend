349fcbb518d7980d10243834883bf547b170cf08
ZainUlBasit <zainulbasit486@gmail.com>
2024-09-02 16:38:16 +0500

    chnnages

--- src/pages/Cash Payments/FixedAssets.jsx
+++ src/pages/Cash Payments/FixedAssets.jsx
@@ -12,6 +12,14 @@ const FixedAssets = () => {
     <div className="relative">
       <Navbar />
       <CashPaymentNav />
+      <div className="">
+        <div className="bg-[#464F51] border-2 border-[#464F51] hover:bg-[#464f51c7] text-white px-3 py-3 text-xl font-bold hover:rounded-[10px] transition-all ease-in-out duration-500 cursor-pointer">
+          Add Fixed Assets
+        </div>
+        <div className="bg-[#464F51] border-2 border-[#464F51] hover:bg-[#464f51c7] text-white px-3 py-3 text-xl font-bold hover:rounded-[10px] transition-all ease-in-out duration-500 cursor-pointer">
+          Add Amount
+        </div>
+      </div>
       <TableWrapper>
         <Search
           Placeholder={"Search Assets..."}
