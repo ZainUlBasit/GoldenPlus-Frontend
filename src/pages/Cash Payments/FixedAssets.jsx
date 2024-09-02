1aec497171d1bdcdc26308977f2936fb28d2afda
ZainUlBasit <zainulbasit486@gmail.com>
2024-09-02 15:51:32 +0500

    cs

--- src/pages/Cash Payments/FixedAssets.jsx
+++ src/pages/Cash Payments/FixedAssets.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import CashPaymentNav from "../../components/Navigations/CashPaymentNav";
 import TableWrapper from "../../components/Tables/TableWrapper";
