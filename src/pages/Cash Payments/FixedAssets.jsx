a9e79e65ec843ec56df9b79c8b993b29ff5d97ca
ZainUlBasit <zainulbasit486@gmail.com>
2024-09-02 15:50:01 +0500

    chnages

--- src/pages/Cash Payments/FixedAssets.jsx
+++ src/pages/Cash Payments/FixedAssets.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import CashPaymentNav from "../../components/Navigations/CashPaymentNav";
+import TableWrapper from "../../components/Tables/TableWrapper";
+import Search from "../../components/SearchBox/Search";
+import FixedAssetsInfoTable from "../../components/Tables/FixedAssetsInfoTable";
 
 const FixedAssets = () => {
+  const [SearchText, setSearchText] = useState("");
+
   return (
     <div className="relative">
       <Navbar />
       <CashPaymentNav />
-      <div>FixedAssets</div>
+      <TableWrapper>
+        <Search
+          Placeholder={"Search Assets..."}
+          Value={SearchText}
+          setValue={setSearchText}
+        />
+        <FixedAssetsInfoTable Rows={[{}]} />
+      </TableWrapper>
     </div>
   );
 };
---
+++ src/utils/ColumnsData/AssetsColumns.jsx
@@ -0,0 +1,5 @@
+export const AssetsColumns = [
+  { title: "Name", id: "name" },
+  { title: "Amount", id: "amount" },
+  { title: "Branch Name", id: "branch_name" },
+];
