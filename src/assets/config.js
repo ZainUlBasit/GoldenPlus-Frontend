704419ad092dcd6f28bd546be36e92b673760b79
ZainUlBasit <zainulbasit486@gmail.com>
2025-05-04 14:08:44 +0500

    qwerty

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,3 +1,3 @@
 export const BASE_URL =
   "https://goldenplus-backend-production.up.railway.app/api";
-export const BASE_URL_LOCAL = "http://localhost:8000/api";
+export const BASE_URL_LOCAL = "http://localhost:8004/api";
